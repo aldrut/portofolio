1f013f359c972c2bd8842153efd5f8249e8e963d
aldrut <a.drut@free.fr>
2021-07-12 09:12:47 +0200

    ajout exo.js

--- js/portofolio.js
+++ js/portofolio.js
@@ -1,4 +1,8 @@
-function sendEmail()
-{
-    window.open('mailto:a.drut@free');
-}
+var labelDate = document.querySelector("#lblDate");
+
+/*Affichage de la date du jour*/ 
+
+var dateOfDay= new Date(2021,03,22);
+
+labelDate.innerHTML = dateOfDay;
+
